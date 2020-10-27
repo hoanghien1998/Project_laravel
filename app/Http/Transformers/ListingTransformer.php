5b6af096f0bc8f552c6417b10aa7e0033046dbfd
Ngoc Le <ngoc.le@saritasa.com>
2020-10-27 16:33:00 +0700

    Get listing by id W13D1

--- app/Http/Transformers/ListingTransformer.php
+++ app/Http/Transformers/ListingTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Transformers;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Saritasa\Transformers\BaseTransformer;
 
 /**
@@ -9,4 +10,20 @@ use Saritasa\Transformers\BaseTransformer;
  */
 class ListingTransformer extends BaseTransformer
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(Arrayable $model): array
+    {
+        $data = parent::transform($model);
+        $data['medias'] = $model->documents->filter(function ($document) {
+            return $document->group == 'image';
+        })->map(function ($document) {
+            return [
+                'thumbnail' => $document->thumbnail,
+                'full' => $document->full,
+            ];
+        });
+        return $data;
+    }
 }
